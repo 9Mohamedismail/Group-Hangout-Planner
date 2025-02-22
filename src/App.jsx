11a44caca3a3d2ea98004b5acdda2617cd4e4a84
jerrry-10 <78663777+Jerrry-10@users.noreply.github.com>
2025-02-22 13:24:59 -0500

    Add NavBar component and update HomePage layout

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,12 @@ import HomePage from "./pages/HomePage";
 import GroupHangoutJoinPage from "./pages/GroupHangoutJoinPage";
 import GroupHangoutSessionPage from "./pages/GroupHangoutSessionPage";
 import CreateSessionField from "./components/CreateSessionField";
+import NavBar from "../src/components/navbar";
 
 function App() {
   return (
     <div>
+      <NavBar />
       <Routes>
         <Route path="join/:sessionId" element={<GroupHangoutJoinPage />} />
         <Route path="/" element={<HomePage />} />
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,9 +2,18 @@ import { useNavigate } from "react-router-dom";
 
 function HomePage() {
   const navigate = useNavigate();
+
   return (
-    <div>
-      <button onClick={() => navigate("/create")}>Open Create Session</button>
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+      <h1 className="text-2xl font-bold text-center">
+        PLAN TOGETHER <br></br>STAY ORGANIZED
+      </h1>
+      <button
+        className="bg-blue-500 text-white px-6 py-2 rounded hover:bg-blue-600 mt-4"
+        onClick={() => navigate("/create")}
+      >
+        CREATE EVENT
+      </button>
     </div>
   );
 }
